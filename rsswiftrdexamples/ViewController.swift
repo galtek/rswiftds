0da35e0b5dfbd837a8675a5d443bbe0eeadec627
Rafael <rrodriguez@hoyosintegrity.com>
2020-04-12 20:42:45 -0400

    Initial project setup

---
+++ rsswiftrdexamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rsswiftrdexamples
+//
+//  Created by Rafael Rodriguez on 4/12/20.
+//  Copyright © 2020 Rafael Adames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
