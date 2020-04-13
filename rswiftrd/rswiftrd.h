a4ed6e930825cee05f157cedb9e42ca8b5a94b7b
Rafael <rrodriguez@hoyosintegrity.com>
2020-04-12 20:28:13 -0400

    Initial Commit

---
+++ rswiftrd/rswiftrd.h
@@ -0,0 +1,19 @@
+//
+//  rswiftrd.h
+//  rswiftrd
+//
+//  Created by Rafael Rodriguez on 4/12/20.
+//  Copyright © 2020 Rafael Adames. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for rswiftrd.
+FOUNDATION_EXPORT double rswiftrdVersionNumber;
+
+//! Project version string for rswiftrd.
+FOUNDATION_EXPORT const unsigned char rswiftrdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rswiftrd/PublicHeader.h>
+
+
